381a5f786ff25ea3cb6f7856bb25ec7891163326
laopeng <521134148899>
2020-05-04 21:44:12 +0800

    init app

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <router-view></router-view>
+    <Footer v-show="!$route.meta.isHideFooter" />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+  },
+};
+</script>
+
+<style scoped></style>
